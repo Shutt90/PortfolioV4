6934ec11c585b3e2df2edda2cc1352048d75aaee
Shutt90 <78647249+Shutt90@users.noreply.github.com>
2022-02-07 13:51:07 +0000

    Passing data from mongodb to front end

--- pages/api/projects/index.js
+++ pages/api/projects/index.js
@@ -7,7 +7,6 @@ const prisma = new PrismaClient()
 export default async (req, res) => {
   try {
     await prisma.$connect()
-    const projects = await prisma.projects.findMany()
 
     await prisma.projects.create({
     data: {
@@ -17,11 +16,23 @@ export default async (req, res) => {
         images: req.body.images,
       },
     })
-    res.status(200).render({message: 'Uploaded project'})
+    res.status(200).json({message: 'Uploaded project'})
   } catch(err) {
     console.log(err)
     res.status(500).json({error: 'Please try again later'})
   } finally {
     await prisma.$disconnect()
   }
+}
+
+export async function getServerSideProps() {
+  const projects = await prisma.projects.findMany()
+  return {
+    props: {
+      projects: projects
+
+    }
+
+  }
+
 }
--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import Form from '../../components/Form';
 import Layout from '../../components/Layout';
+import Textblock from '../../components/Textblock'
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
-function index() {
+function index({projects}) {
 
+  const stringLimit = 10
   return (
       <Layout>
+        <div className="flex">
+          {projects.map((project) => {
+            return <Textblock key={project.id} subtitle={project.title} paragraph={project.body.length > stringLimit ? project.body.substring(0, stringLimit) + '...' : project.body } />
+          })}
+        </div>
+          
           <Form />
+
       </Layout>
+      
   )
 }
+
+export async function getServerSideProps() {
+  const projects = await prisma.projects.findMany();
+  return {
+    props: {
+      projects: projects,
+    },
+  };
+}
+
 
 export default index;
