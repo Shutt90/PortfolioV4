a31aac70993393b22a26ee6686e1c2224fc76ce7
Shutt90 <78647249+Shutt90@users.noreply.github.com>
2022-03-08 22:10:49 +0000

    Testing gtag

--- containers/Layout.js
+++ containers/Layout.js
@@ -3,7 +3,7 @@ import Footer from '/components/Footer';
 import Head from 'next/head';
 import React from 'react';
 import { useRouter } from 'next/router';
-
+import Script from 'next/script';
 
 
 function Layout({children}) {
@@ -18,6 +18,18 @@ function Layout({children}) {
         <meta name="title" content="Web Developer" />
         <meta name="description" content="Web Developer based in Manchester working with JavaScript and PHP" />
         <link rel="icon" href="/favicon.ico" />
+        <>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-48S1TTWQQT" />
+        <Script id="gtag"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '[G-48S1TTWQQT]', { page_path: window.location.pathname });
+            `,
+          }} />
+        </>
       </Head>
       <Navbar/>
             {children}
