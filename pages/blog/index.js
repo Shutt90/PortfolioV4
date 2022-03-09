48d88a431bcd9f53a0585715330d5d7fd9b45315
Shutt90 <78647249+Shutt90@users.noreply.github.com>
2022-03-09 17:26:32 +0000

    Brought project form onto blog too

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,5 +1,5 @@
 import { motion } from 'framer-motion';
-import React from 'react';
+import React, { useState } from 'react';
 import Form from '../../components/Form';
 import BlogPost from '/components/BlogPost';
 import Layout from '/containers/Layout';
@@ -7,10 +7,48 @@ import Link from 'next/link';
 import loadPosts from '/lib/load-posts'
 
 export default function Index({posts}) {  
+
+  const [query, setQuery] = useState("");
+  const [formOpen, setFormOpen] = useState(0);
+  const [displayPlusX, setDisplayPlusX] = useState(0);
+  const [displayPlusOpacity, setDisplayPlusOpacity] = useState(1);
+  const [displayMinusX, setDisplayMinusX] = useState(0);
+  const [displayMinusOpacity, setDisplayMinusOpacity] = useState(0);
+
+  function plusOnClick () {
+    setDisplayPlusX(50)
+    setDisplayPlusOpacity(0)
+    setDisplayMinusX(0)
+    setDisplayMinusOpacity(1)
+    setFormOpen(600)
+  }
+
+  function minusOnClick () {
+    setDisplayPlusX(0)
+    setDisplayPlusOpacity(1)
+    setDisplayMinusX(50)
+    setDisplayMinusOpacity(0)
+    setFormOpen(0)
+
+  }
   
   return (
     <Layout>
-      <Form className='' initial={{opacity: 0}} action='POST'/>
+      <div className="flex-column align-center">
+        <motion.img onClick={(e) => formOpen === 0 ? plusOnClick() : minusOnClick()} intial={{x: 0, opacity: displayPlusOpacity}} animate={{x: displayPlusX + 'px', opacity: displayPlusOpacity}}
+        src="/static/icons8-plus-math-64.png"
+        style={{width: "40px", height: "40px", margin: "auto"}}>
+        </motion.img>
+        <motion.img onClick={(e) => formOpen === 0 ? plusOnClick() : minusOnClick()} initial ={{opacity: 0}}animate={{
+          x: displayMinusX + 'px',
+          opacity: displayMinusOpacity,}}
+        src="/static/icons8-minus-64.png"
+        style={{width: "40px", height: "40px", margin: "auto"}}>
+        </motion.img>
+        <motion.div style={{overflow: 'hidden'}} initial={{x: 0, height: 0}} animate={{height: formOpen + 'px'}}>
+          <Form className='' initial={{opacity: 0}} action='POST'/>
+        </motion.div>
+      </div>
       <motion.div >
       {posts.map((post) => {
             return (
