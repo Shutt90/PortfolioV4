3ffd1a8b751cad5fa362add8d2bcf69e3d7b5c2e
Shutt90 <78647249+Shutt90@users.noreply.github.com>
2022-03-11 19:55:37 +0000

    Finished strava contact icon/anim

--- components/StravaIcon.js
+++ components/StravaIcon.js
@@ -12,9 +12,11 @@ function LinkedinIcon({className}) {
   }
 
   return (
-    <motion.svg className={className} whileHover={setStrava} viewBox="0 0 63 63" xmlns="http://www.w3.org/2000/svg" >
-      <motion.path d="M41.03 47.852l-5.572-10.976h-8.172L41.03 64l13.736-27.124h-8.18" fill={topColor} /><motion.path d="M27.898 21.944l7.564 14.928h11.124L27.898 0 9.234 36.876H20.35" fill={bottomColor}/>
-    </motion.svg>
+    <a href="https://www.strava.com/athletes/68916308">
+      <motion.svg className={className} whileHover={setStrava} viewBox="0 0 63 63" xmlns="http://www.w3.org/2000/svg" >
+        <motion.path d="M41.03 47.852l-5.572-10.976h-8.172L41.03 64l13.736-27.124h-8.18" fill={topColor} /><motion.path d="M27.898 21.944l7.564 14.928h11.124L27.898 0 9.234 36.876H20.35" fill={bottomColor}/>
+      </motion.svg>
+    </a>
     )
 }
 
