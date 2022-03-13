2a3f44a10e904d49cec9ac1997dd907ec20a72a0
Liam Pugh <liam.pugh.009@gmail.com>
2022-03-13 23:38:08 +0000

    Slight adjustment to homepage

--- components/Banner.js
+++ components/Banner.js
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 import styles from '/styles/banner.module.css'
 function Banner({title, subtitle, alt, onMouseEnter, onMouseLeave}) {
@@ -7,6 +8,7 @@ function Banner({title, subtitle, alt, onMouseEnter, onMouseLeave}) {
         <div className={styles.textarea}>
             <h1 className={styles.title}>{title}</h1>
             <h3 className={styles.subtitle}>{subtitle}</h3>
+            <Link href="/contact"><a class="standard-btn" style={{width: '70%'}}>Get in touch...</a></Link>
         </div>
         <Image className={styles.bannerImage} alt={alt} width='400' height='400' src='/static/js.png' />
     </div>
